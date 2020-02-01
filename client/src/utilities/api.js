41794373d3a3832370ab4be44313ba0481f18ae7
jmbodnar <jmbodnar@protonmail.com>
2020-02-01 17:50:18 -0500

    Mirror develop

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -1,6 +1,6 @@
 export async function getRecipes() {
   try {
-    return await (await fetch("http://localhost:3000/api/recipes")).json();
+    return await (await fetch("http://localhost:3001/api/recipes")).json();
   } catch (error) {
     console.error(error);
   }
@@ -8,7 +8,7 @@ export async function getRecipes() {
 
 export async function getRecipeId(id) {
   try {
-    return await (await fetch(`http://localhost:3000/api/recipe/${id}`)).json();
+    return await (await fetch(`http://localhost:3001/api/recipe/${id}`)).json();
   } catch (error) {
     console.error(error);
   }
