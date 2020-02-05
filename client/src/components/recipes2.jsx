52b86b0498b2e4ee2c0db9fda3c4a069982418d3
Aruna Kolli <Kolli_aruna2006@yahoo.co.in>
2020-02-04 21:15:34 -0500

    Add datatables

--- client/src/components/recipes2.jsx
+++ client/src/components/recipes2.jsx
@@ -2,25 +2,63 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { getRecipes } from "../utilities/api";
 import PageHeader from "./page-header";
+import Datatable from "./datatables";
+import { MDBDataTable } from "mdbreact";
+const $ = require("jquery");
+$.DataTable = require("datatables.net");
 
 class Recipes2 extends Component {
   state = {
+    data: {},
     recipes: []
   };
+  config = [
+    {
+      label: "Title",
+      field: "title",
+      sort: "asc",
+      width: 150
+    },
+    {
+      label: "Category",
+      field: "category",
+      sort: "asc",
+      width: 270
+    },
+    {
+      label: "User",
+      field: "user[firstname]",
 
-  componentDidMount() {
+      sort: "asc",
+      width: 200
+    },
+    {
+      label: "Likes",
+      field: "likes",
+      sort: "asc",
+      width: 200
+    }
+  ];
+
+  componentDidMount = () => {
+    console.log("1", this);
     getRecipes().then(recipes => {
-      this.setState({ recipes });
+      console.log("2", this);
+      this.setState({ recipes: recipes });
+      this.setState({ data: { rows: recipes, columns: this.config } });
     });
-  }
+  };
 
   render() {
-    const { recipes } = this.state;
+    // const { recipes } = this.state;
     return (
       <React.Fragment>
         <PageHeader title="All Recipes" />
         <div className="table-responsive">
-          <table className="table table-striped table-borderless table-hover">
+          {/* <table
+            id="table_id"
+            className="table table-striped table-borderless table-hover"
+          >
             <thead className="thead-light">
               <tr>
                 <th>Title</th>
@@ -30,21 +68,28 @@ class Recipes2 extends Component {
               </tr>
             </thead>
             <tbody>
-              {recipes.map(r => {
-                return (
-                  <tr key={r._id}>
-                    <td>
-                      <Link to={"recipe/" + r._id}>{r.title}</Link>
-                    </td>
-                    <td>{r.category}</td>
-                    <td>{r.user.firstname + " " + r.user.lastname}</td>
-                    <td>{r.likes}</td>
-                  </tr>
-                );
-              })}
+              {this.state.recipes.length
+                ? this.state.recipes.map(r => {
+                    return (
+                      <tr key={r._id}>
+                        <td>
+                          <Link to={"recipe/" + r._id}>{r.title}</Link>
+                        </td>
+                        <td>{r.category}</td>
+                        <td>{r.user.firstname + " " + r.user.lastname}</td>
+                        <td>{r.likes}</td>
+                      </tr>
+                    );
+                  })
+                : "swhere them recipes"}
             </tbody>
-          </table>
+          </table> */}
+          <MDBDataTable data={this.state.data} />
         </div>
+        <script>
+          document.addEventListener("DOMContentLoaded", function()
+          {/* $("#table_id").DataTable() }); */}
+        </script>
       </React.Fragment>
     );
   }
--- client/src/components/user-form.jsx
+++ client/src/components/user-form.jsx
@@ -1,15 +1,108 @@
-import React, { Component } from 'react';
-import PageHeader from './page-header';
+import React, { Component } from "react";
+import PageHeader from "./page-header";
+import axios from "axios";
 
 class UserForm extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <React.Fragment>
-        <PageHeader title="Add Yourself!" />
-      </React.Fragment>
-     );
+  state = {
+    first_name: "",
+    last_name: "",
+    email: ""
+  };
+  handleInputChange = event => {
+    console.log("I was changed");
+    const { name, value } = event.target;
+    // console.log("target hapepend: ", event.target)
+    this.setState({
+      [name]: value
+    });
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log("I did submit");
+    console.log(`Your first name:${this.state.first_name}`);
+    console.log(`Your last name:${this.state.last_name}`);
+    console.log(`Your email:${this.state.email}`);
+
+    const user = {
+      firstname: this.state.first_name,
+      lastname: this.state.last_name,
+
+      email: this.state.email
+    };
+    axios
+      .post("http://localhost:3001/api/users", user)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .then(() => {
+        this.props.history.replace("/");
+      });
+  };
+  render() {
+    return (
+      // <React.Fragment>
+      //   <PageHeader title="Add Your Recipe!" />
+      // </React.Fragment>
+
+      /* // adding html form */
+      <div className="container my-5">
+        <h2>Become A User</h2>
+        <form onSubmit={this.handleFormSubmit}>
+          <fieldset>
+            <legend>Your Information</legend>
+            <div className="row">
+              <div className="col-6">
+                <div className="form-group">
+                  <label for="first_name">First Name</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="first_name"
+                    name="first_name"
+                    placeholder="First name"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="form-group">
+                  <label for="last_name">Last Name</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="last_name"
+                    name="last_name"
+                    placeholder="Last name"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label for="email">Email</label>
+              <input
+                className="form-control"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+          </fieldset>
+
+          <input
+            type="submit"
+            value="Become A Member"
+            className="btn btn-info btn-block"
+          />
+        </form>
+      </div>
+    );
   }
 }
- 
 export default UserForm;
