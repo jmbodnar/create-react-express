158e2b8851317d075a38923d5827f6b3548174c5
jmbodnar <jmbodnar@protonmail.com>
2020-02-08 06:15:00 -0500

    Add user info to session storage

--- client/src/utilities/general.js
+++ client/src/utilities/general.js
@@ -2,3 +2,8 @@ export function jsonToDateString(dateJSON) {
   const date = new Date(dateJSON);
   return date.toLocaleDateString();
 }
+
+export function checkStorage(inputName) {
+  let user = JSON.parse(sessionStorage.getItem(user));
+  console.log(user);
+}
