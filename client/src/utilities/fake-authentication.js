b9476fa5ec29cdf36958e35642c12cfded5f31d2
jmbodnar <jmbodnar@protonmail.com>
2020-02-08 05:59:40 -0500

    Switch to session storage

--- client/src/utilities/fake-authentication.js
+++ client/src/utilities/fake-authentication.js
@@ -1,13 +1,13 @@
 export const fakeAuth = {
   isAuthenticated() {
-    return JSON.parse(localStorage.getItem("isAuthenticated"));
+    return JSON.parse(sessionStorage.getItem("isAuthenticated"));
   },
   authenticated(cb) {
-    localStorage.setItem("isAuthenticated", "true");
+    sessionStorage.setItem("isAuthenticated", "true");
     setTimeout(cb, 1000); // Fake an async call
   },
   signout(cb) {
-    localStorage.setItem("isAuthenticated", "false");
+    sessionStorage.setItem("isAuthenticated", "false");
     setTimeout(cb, 1000); // Fake an async call
   }
 };
